d24ad2ceb6d7dd214dd7c45b6c5069204b899ceb
btw-saanvi <Saanvigarg2020@gmail.com>
2025-02-28 10:32:10 +0530

    Completed the assignment

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,28 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 
+// const App = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Dashboard />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Dashboard from './components/Dashboard';
+import AddMovie from './components/AddMovie';
+
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/add-movie" element={<AddMovie />} />
       </Routes>
     </Router>
   );
